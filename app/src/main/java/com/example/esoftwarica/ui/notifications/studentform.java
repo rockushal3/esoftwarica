26a8b126b40c66f2b2ba39226dc4a50451204570
kushal shrestha <rockushal3@gmail.com>
2019-11-18 18:44:29 +0545

    Initial commit

---
+++ app/src/main/java/com/example/esoftwarica/ui/notifications/studentform.java
@@ -0,0 +1,101 @@
+package com.example.esoftwarica.ui.notifications;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.esoftwarica.R;
+import com.example.esoftwarica.ui.home.HomeFragment;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class studentform extends Fragment implements View.OnClickListener,RadioGroup.OnCheckedChangeListener{
+
+    Button submit;
+    RadioGroup gender;
+    RadioButton male,female;
+    EditText name, age,address;
+    public static List<Student> details = new ArrayList<>();
+
+
+    String uname,uage,ugender,uaddress;
+    private NotificationsViewModel notificationsViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        notificationsViewModel =
+                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+        View root = inflater.inflate(R.layout.studentform, container, false);
+
+        submit = root.findViewById(R.id.submit);
+        gender = root.findViewById(R.id.gender);
+        male =root.findViewById(R.id.male);
+        female = root.findViewById(R.id.female);
+        name = root.findViewById(R.id.name);
+        age =root.findViewById(R.id.age);
+        address=root.findViewById(R.id.address);
+        submit.setOnClickListener(this);
+        gender.setOnCheckedChangeListener(this);
+
+
+        notificationsViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+
+                gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                        if (i == R.id.male) {
+                            ugender = "Male";
+                            Toast.makeText(getContext(), "Male", Toast.LENGTH_SHORT).show();
+                        }
+                        if (i == R.id.female) {
+                            ugender = "Female";
+                            Toast.makeText(getContext(), "Female", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+                submit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                         uage = age.getText().toString();
+                         uname = name.getText().toString();
+                         uaddress = address.getText().toString();
+                        details.add(new Student(uname,ugender,uage,uaddress));
+
+
+                    }
+                });
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+    }
+}
